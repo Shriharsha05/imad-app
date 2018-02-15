12d22136ce0088279ed9f18a5944916de1467d1a
Shriharsha M <shriharsha05@gmail.com>
2018-02-15 18:08:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,18 @@
 console.log('Loaded!');
 
-
+/*  // to change html main text
 var elemnt = document.getElementById('main-txt');
 elemnt.innerHTML = 'abcd';
+*/
+
+var img = document.getElementById('madi');
+var marginLeft=0;
+
+function moveRight(){
+    marginLeft + = 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function(){
+    var interval = setInterval(moveRight,50);
+}
