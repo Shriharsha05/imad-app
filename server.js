77ce36c4a8594893786ba065066e6dcd1e7caa9e
Shriharsha M <shriharsha05@gmail.com>
2018-02-15 17:17:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-var articleOne = {
+var articles = {
+  'article-one' : {
   heading : 'Key Loggers',
   date : '22 December 2016',
   content:`<p>Key loggers are the programs which record each and every keystroke on the computer they are installed on.It can store a complete log of text entered such as passwords,text copied and pasted,websites visited etc.User wont be aware of this key-spying software.Its usually used in cyber to get passwords of users and misuse it. Key-logger monitor screen will be opened only when particular 3 or 4 key combination will be pressed simultaneously.
 		</p>` 
-}; 
+        },
+  'article-two' : {heading : 'Whatsapp Spam',
+  date : '1 November 2016',
+  content:`<p> FEEL THE POWER OF PYTHON!!
+		</p>` 
+      
+  }        
+};
+ 
 
 function createTemplate(data){
     var heading = data.heading;
@@ -63,13 +71,16 @@ app.get('/hello', function(req,res) {
  res.sendFile(path.join(__dirname,'ui','hello.html'));   
 });
 
-app.get('/article-one', function(req,res) {
- res.send(createTemplate(articleOne));   
+app.get('/:articleName', function(req,res) {
+    var articleName = req.params.articleName;
+ res.send(createTemplate(articles[articleName]));   
 });
 
+/*
 app.get('/article-two', function(req,res) {
  res.send("will be hosted shortly");   
-});
+});*/
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
