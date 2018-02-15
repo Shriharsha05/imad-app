8a8b0ac1918f89341b77a817f6da1859a2795651
Shriharsha M <shriharsha05@gmail.com>
2018-02-16 00:01:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ btn.onclick = function() {
         
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status===200){
-                var counter = request.responceText;
+                var counter = request.responseText;
                  var val = document.getElementById('count');
                  val.innerHTML=counter.toString();
             }
