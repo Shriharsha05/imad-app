c1ecc9426d39637ab5112e3d15a869b092827717
Shriharsha M <shriharsha05@gmail.com>
2018-02-15 17:44:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/hello', function(req,res) {
  res.sendFile(path.join(__dirname,'ui','hello.html'));   
 });
